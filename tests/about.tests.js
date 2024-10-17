b77675ff21ba48255f436ac2d9803cf1735f3049
Nikolay <115391462+manchev7717@users.noreply.github.com>
2024-10-18 00:47:31 +0300

    comment for testing

--- tests/about.tests.js
+++ tests/about.tests.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const fetch = require('node-fetch');
 
+//add some comment for testing
 suite('About page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8888/about");
